54eacb50b1d7dcd8b9f93f0189e509f57dedffb8
João Macaoli <devjoaom@gmail.com>
2024-09-05 12:49:51 -0300

    set type any on error flag

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ export default function Login() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     const env = process.env.NEXT_PUBLIC_API_PATH
